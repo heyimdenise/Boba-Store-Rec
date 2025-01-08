402664d367877b1865bce3b45deae7785259ab2a
heyimdenise <161521362+heyimdenise@users.noreply.github.com>
2025-01-07 16:10:11 -0800

    Add files via upload

--- random_boba.py
+++ random_boba.py
@@ -23,6 +23,20 @@ def search_store_on_yelp(location):
         print(f"Error: {response.status_code} - Unable to retrieve data. Please check your network connection and try again")
         return []
 
+def get_reviews(business_id):
+    API_Key = "xgzrxqPH6jTbrs4NWK7WANs1SrFJKtPg57m5L2aSpQAV0mWH839-VUjxq1psmTpi_2D3mDDC2Qi-VXbt2jenLa8MaGmsMOjyruOXvygjRhdicYKwIR3JgemMZZh9Z3Yx"
+    headers = {"Authorization": f"Bearer {API_Key}"}
+    url = f"https://api.yelp.com/v3/businesses/{business_id}/reviews"
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        reviews = [review['text'].lower() for review in data['reviews']]
+        return reviews
+    else:
+        print(f"Error: Unable to retrieve reviews for business ID: {business_id}")
+        return []
 
 def boba_picker():
     print("Welcome to the Automatic Deicision Maker: Boba Store Edition!")
@@ -35,22 +49,26 @@ def boba_picker():
         print("\nNo boba stores found. Please try again.")
         return
 
-
     print(f"Found {len(boba_stores)} boba stores in {location}.")
 
     # ask user if they are craving a specific drink
     craving = input("\nAre you craving a specific drink (e.g., matcha, green milk tea, taro)? If not, press 'Enter' to skip: ").lower().strip()
 
     if craving: # if the user is craving something specific
-        filtered_stores = [
-            store for store in boba_stores
-            if craving in store['name'].lower() or craving in ', '.join(cat['title'].lower() for cat in store['categories'])
-        ]
+        filtered_stores = []
+        for store in boba_stores:
+            print(f"Fetching reviews for: {store['name']}, Business ID: {store['id']}")
+            # get reviews for the store using reviews api which has more specific drink names
+            reviews = get_reviews(store['id'])  # retrieve top 3 reviews
+            if any(craving in review for review in reviews):
+                filtered_stores.append(store)
+                print(f"'{craving}' found in reviews for: {store['name']}!")
+
         if filtered_stores: # if the stores match the craving
             chosen_store = random.choice(filtered_stores)
-            print(f"\nYou should go toooo {chosen_store['name']} for your craving")
+            print(f"\nYou should go to {chosen_store['name']} for your craving!")
         else:   # if no store matches the craving
-            print(f"\nSorry, no boba store mentions '{craving}'. Let's pick from what is nearby.")
+            print(f"\nSorry, no boba store mentions '{craving}'. Let's pick from the list.")
             random_choice = random.choice(boba_stores)  
             print(f"\nYou should try {random_choice['name']}!")
     else:   # if the user has no cravings
